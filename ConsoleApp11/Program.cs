97a5d4730c7056b287d952c962b06d7031f8ffd0
unknown <947044537@qq.com>
2024-03-14 17:24:18 +0800

    提交一个sln

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,13 @@
+﻿using QuoteDllSay;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tempclass = new SayHello2();
+            tempclass.SayHello();
+        }
+    }
+}
